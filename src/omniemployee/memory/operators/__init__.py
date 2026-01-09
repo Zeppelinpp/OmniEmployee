b13163bd20a204d87bbc0beac3fe40321f0eb9a8
Zeppelinpp <1056118649@qq.com>
2026-01-09 13:40:22 +0800

    remove heuristic_conflict_check and use llm conflict check

--- src/omniemployee/memory/operators/__init__.py
+++ src/omniemployee/memory/operators/__init__.py
@@ -3,7 +3,11 @@
 from src.omniemployee.memory.operators.energy import EnergyController, EnergyConfig
 from src.omniemployee.memory.operators.encoder import Encoder, EncoderConfig
 from src.omniemployee.memory.operators.router import AssociationRouter, RouterConfig
-from src.omniemployee.memory.operators.conflict import ConflictChecker, ConflictConfig
+from src.omniemployee.memory.operators.conflict import (
+    ConflictChecker,
+    ConflictConfig,
+    create_llm_conflict_callback,
+)
 
 __all__ = [
     "EnergyController",
@@ -14,4 +18,5 @@ __all__ = [
     "RouterConfig",
     "ConflictChecker",
     "ConflictConfig",
+    "create_llm_conflict_callback",
 ]
